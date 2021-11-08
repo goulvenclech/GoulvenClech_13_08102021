609ce633e91f037bf8622417d611bcd06d8955bd
Goulven Nev <nevilain@gmail.com>
2021-11-08 10:41:30 +0100

    draft login

---
+++ src/Components/SignIn/LoginForm.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react"
+import { useHistory } from "react-router-dom"
+/**
+ * The sign in form of our bank. 
+ */
+export default function LoginForm() {
+  // Change while the use is typing
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  // Needed for handleSubmit
+  const [error, setError] = useState("")
+  const history = useHistory()
+  // handleSubit post request on login back end
+  const handleSubmit = (evt: React.SyntheticEvent) => {
+    evt.preventDefault()
+    fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ 
+        email: username,
+        password: password 
+      })
+    })
+    .then(async response => {
+      const isJson = response.headers.get("content-type")?.includes("application/json")
+      const data = isJson && await response.json()
+      if (response.ok) {
+        console.log(response)
+        history.push("/user")
+      } else {
+        console.error(response)
+        setError(data.message)
+      }
+    })
+    .catch(error => {
+      console.error(error)
+      setError(error)
+    })
+  }
+  // a simple login form 
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-wrapper">
+      {
+        error === "" ?
+        "" :
+        <p>{error}</p>
+      }
+      <label htmlFor="username">
+        Username
+      </label>
+      <input type="text" id="username"
+        onChange={e => setUsername(e.target.value)} />
+      </div>
+      <div className="input-wrapper">
+      <label htmlFor="password">
+        Password
+      </label>
+      <input type="password" id="password"
+        onChange={e => setPassword(e.target.value)} />
+      </div>
+      <div className="input-remember">
+      <label htmlFor="remember-me">
+        Remember me
+      </label>
+      <input type="checkbox" id="remember-me" />
+      </div>
+      <button type="submit" className="sign-in-button">
+        Sign In
+      </button>
+    </form>
+  )
+}
--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -1,5 +1,8 @@
 import React from "react"
-
+import LoginForm from "../Components/SignIn/LoginForm"
+/**
+ * Contain a form to log in our app 
+ */
 export default function SignInPage() {
   return (
     <main className="main bg-dark">
@@ -8,29 +11,7 @@ export default function SignInPage() {
         <h1>
           Sign In
         </h1>
-        <form>
-          <div className="input-wrapper">
-            <label htmlFor="username">
-              Username
-            </label>
-            <input type="text" id="username" />
-          </div>
-          <div className="input-wrapper">
-            <label htmlFor="password">
-              Password
-            </label>
-            <input type="password" id="password" />
-          </div>
-          <div className="input-remember">
-            <label htmlFor="remember-me">
-              Remember me
-            </label>
-            <input type="checkbox" id="remember-me" />
-          </div>
-          <a className="sign-in-button">
-            Sign In
-          </a>
-        </form>
+        <LoginForm />
       </section>
     </main>
   )
